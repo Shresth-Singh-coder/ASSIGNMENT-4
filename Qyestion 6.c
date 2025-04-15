2ce78b1608189bddd4f3c4bec396062a594b53c8
Shresth-Singh-coder <shresthengineer@gmail.com>
2025-04-15 22:10:32 +0530

    Create Qyestion 6.c

---
+++ Qyestion 6.c
@@ -0,0 +1,82 @@
+Implement bubble sort in singly linked list.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void append(struct Node** headRef, int data) {
+    struct Node* newNode = createNode(data);
+    if (*headRef == NULL) {
+        *headRef = newNode;
+        return;
+    }
+    struct Node* temp = *headRef;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int getLength(struct Node* head) {
+    int length = 0;
+    while (head != NULL) {
+        length++;
+        head = head->next;
+    }
+    return length;
+}
+
+void bubbleSort(struct Node* head) {
+    if (head == NULL) return;
+    int swapped, i;
+    struct Node *ptr1;
+    struct Node *lptr = NULL;
+    do {
+        swapped = 0;
+        ptr1 = head;
+        while (ptr1->next != lptr) {
+            if (ptr1->data > ptr1->next->data) {
+                int temp = ptr1->data;
+                ptr1->data = ptr1->next->data;
+                ptr1->next->data = temp;
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+}
+
+int main() {
+    struct Node* head = NULL;
+    append(&head, 30);
+    append(&head, 10);
+    append(&head, 40);
+    append(&head, 20);
+    append(&head, 50);
+    printf("Original list:\n");
+    printList(head);
+    bubbleSort(head);
+    printf("Sorted list (Bubble Sort):\n");
+    printList(head);
+    return 0;
+}
